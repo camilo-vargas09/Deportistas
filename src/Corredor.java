85220a9467266ae7ef568517addcbbda32e1442d
Camilo_Vargas09 <vargascardenaskevincamilo@gmail.com>
2026-08-19 15:36:53 -0500

    clase corredor

--- src/Corredor.java
+++ src/Corredor.java
@@ -1,2 +1,62 @@
-public class Corredor {
+public class Corredor extends Deportista{
+
+    private double distancia;
+    private double tiempo;
+
+    public Corredor() {
+    }
+
+    public Corredor(double distancia, double tiempo) {
+        this.distancia = distancia;
+        this.tiempo = tiempo;
+    }
+
+    public Corredor(String nombre, int edad, String pais, int horasEntrenamiento) {
+        super(nombre, edad, pais, horasEntrenamiento);
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    @Override
+    public String toString() {
+        return "Corredor{" +
+                "distancia=" + distancia +
+                ", tiempo=" + tiempo +
+                ", nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", pais='" + pais + '\'' +
+                ", horasEntrenamiento=" + horasEntrenamiento +
+                '}';
+    }
+
+    public double calcularVelocidad(){
+
+        return distancia/tiempo;
+    }
+
+    @Override
+    public double calcularRendimiento() {
+
+        double velocidad = calcularVelocidad();
+        return velocidad * horasEntrenamiento;
+    }
+
+    public void mostrarDetalles(){
+        System.out.println("Distancia"+ distancia);
+        System.out.println("Tiempo"+ tiempo);
+    }
 }
